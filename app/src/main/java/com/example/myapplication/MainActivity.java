1d1fa2ae4b612c02804d98edb0f147efbc294529
given <makofane411@gmail.com>
2022-01-11 15:08:44 +0200

    creating intent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView drivingSchool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView drivingSchool = findViewById(R.id.textViewDrivingSchool);
+
+        drivingSchool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent drivingSchoolIntent = new Intent(getBaseContext(),drivingSchoolActivity.class);
+                startActivity(drivingSchoolIntent);
+            }
+        });
     }
 }
