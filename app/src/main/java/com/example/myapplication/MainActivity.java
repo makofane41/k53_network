c42bd3fdd85a6a5b51badad5cd358349efad1cde
gven <makofane411>
2022-01-20 16:13:10 +0200

    signIn fixing it

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnNoAccount;
     TextView gotoRegister;
+
+    TextView inputEmail, inputPassword;
+    Button btnLogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    //firebase variable
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +40,15 @@ public class MainActivity extends AppCompatActivity {
         btnNoAccount = findViewById(R.id.btnNoAccount);
         gotoRegister = findViewById(R.id.gotoRegister);
 
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnRegister);
+        progressDialog = new ProgressDialog(this);
+
         gotoRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gotoRegisterIntent = new Intent(getBaseContext(),Register.class);
+                Intent gotoRegisterIntent = new Intent(getBaseContext(), Register.class);
                 startActivity(gotoRegisterIntent);
             }
         });
@@ -32,15 +56,59 @@ public class MainActivity extends AppCompatActivity {
         btnNoAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent btnNoAccountIntent = new Intent(getApplicationContext(),Home.class);
+                Intent btnNoAccountIntent = new Intent(getApplicationContext(), Home.class);
                 startActivity(btnNoAccountIntent);
             }
         });
 
 
+        //initialize firebase
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performLogin();
+            }
+        });
 
 
+    }
 
+    private void performLogin() {
+        String email = inputEmail.getText().toString();
+        //String name = inputName.getText().toString();
+        String password = inputPassword.getText().toString();
 
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("incorrect email!");
+
+        } else {
+            progressDialog.setMessage("Wait, While Loggin!");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(MainActivity.this, "Welcome to K53 Network", Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(MainActivity.this,LoggedHome.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
