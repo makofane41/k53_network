fe0ebf614e7474c859631f416a4f3c33cfe632c4
gven <makofane411>
2022-02-11 13:02:24 +0200

    code elimination

--- app/src/main/java/com/example/myapplication/Account.java
+++ app/src/main/java/com/example/myapplication/Account.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Account extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    Button btnAddDriving;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,15 @@ public class Account extends AppCompatActivity {
         // moveable bottom menu setup
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.account_m);
+        btnAddDriving = findViewById(R.id.btnAddDriving);
+
+        btnAddDriving.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btnAddDrivingIntent = new Intent(getBaseContext(),addDriving.class);
+                startActivity(btnAddDrivingIntent);
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
