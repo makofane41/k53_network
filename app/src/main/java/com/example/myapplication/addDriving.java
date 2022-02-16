939bdd339bac261f3191e2d162a22ca1107cab9c
makofane41 <makofane411@gmail.com>
2022-02-16 19:45:43 +0200

    realDB

--- app/src/main/java/com/example/myapplication/addDriving.java
+++ app/src/main/java/com/example/myapplication/addDriving.java
@@ -3,12 +3,39 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
 
 public class addDriving extends AppCompatActivity {
 
+    EditText drivingName,location,contact;
+    Button add;
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("DrivingSchool");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_driving);
+
+        drivingName = findViewById(R.id.inputDrivingName);
+        location = findViewById(R.id.inputLocation);
+        contact = findViewById(R.id.inputContact);
+        add = findViewById(R.id.btnAdd);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = drivingName.getText().toString();
+                root.setValue(drivingName);
+
+            }
+        });
+
     }
 }
